b3b8e90bd9ff505a67f5855c5fde0cdb035a4572
Ranjith Kumar Reddy <reddyyellareddys@gmail.com>
2020-02-29 11:11:17 +0530

    ranjith

--- MailServer/app.js
+++ MailServer/app.js
@@ -10,11 +10,15 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 3000, function () { 
-  console.log("SERVER STARTED PORT: 3000"); 
-}); 
+// app.listen(process.env.PORT || 3000, function () { 
+//   console.log("SERVER STARTED PORT: 3000"); 
+// }); 
 
-app.get("/", (req, res) => {
+let server = app.listen(3000, function(){
+  let port = server.address().port;
+  console.log("Server started at ", port);
+});
+app.get("https://ranjithmailservice.herokuapp.com", (req, res) => {
   res.send(
     "<h1 style='text-align: center'>Wellcome to FunOfHeuristic <br><br>😃👻😃👻😃👻😃👻😃</h1>"
   );
